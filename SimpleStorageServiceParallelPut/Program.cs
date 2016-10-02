859353cf1e6aceffc81cf8b70485803f747a6493
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-10-02 15:41:07 +0300

    initial

---
+++ SimpleStorageServiceParallelPut/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleStorageServiceParallelPut
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SimpleStorageServiceParallelPut/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleStorageServiceParallelPut")]
+[assembly: AssemblyDescription("Upload files to S3 in parallel")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleStorageServiceParallelPut")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4819dc17-b007-4d3b-b828-14ca4748b777")]
+[assembly: AssemblyVersion("1.0.*")]
